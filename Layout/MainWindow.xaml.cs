a4bca82c42447c0c62603641b088ab7ee70cd56c
denmerc <denmerc@gmail.com>
2014-06-30 17:24:29 -0700

    message center flyout

--- Layout/MainWindow.xaml.cs
+++ Layout/MainWindow.xaml.cs
@@ -25,5 +25,36 @@ namespace Layout
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var flyout = this.Flyouts.Items[0] as Flyout;
+            if (flyout == null)
+            {
+                return;
+            }
+
+            flyout.IsOpen = !flyout.IsOpen;
+
+            Detail.HorizontalAlignment=HorizontalAlignment.Stretch;
+            CommandBar.Width = 400;
+        }
+
+        private void Flyout_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MessageCenter_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+
+        }
+
+        private void MessageCenter_IsOpenChanged(object sender, EventArgs e)
+        {
+            var f = sender as Flyout;
+            if(f != null && f.IsOpen==false)
+                CommandBar.Width = 55;
+        }
     }
 }
